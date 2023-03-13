b398aa23fa60c486756f78f232eb276db40d4d6b
Daniil <d_sirotinin@mail.ru>
2023-03-13 13:02:09 +0300

    First commit

---
+++ Build.bat
@@ -0,0 +1,6 @@
+"D:\Epic Games\UnrealEngine\UE_4.27\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project="D:\UEStudy\Jenkins\TPS\TPS.uproject" ^
+-platform=Win64 -clientconfig=Development ^
+-archivedirectory="D:\UEStudy\Jenkins\TPS\BuildFromBat" ^
+-build -cook -stage -archive -pak -package -allmaps
